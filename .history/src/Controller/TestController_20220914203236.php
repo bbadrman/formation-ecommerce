903debaa3579262ce711582224f9b99abba1f2ad
badrman1983 <bbadrman@gmail.com>
2022-09-14 21:15:39 +0100

    HttpFoundation : Requête et Réponse HTTP !

---
+++ .history/src/Controller/TestController_20220914203236.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+Class TestController 
+{
+    public function index()
+    {
+        var_dump("ca fonctionne bien");
+        die();
+    }
+
+    public function test(){
+        // php native :
+        $age = 0;
+
+        if(!empty($_GET["age"])){
+           $age = $_GET["age"];
+        }
+       dd("test fonctionne bien");
+       
+    }
+} 
