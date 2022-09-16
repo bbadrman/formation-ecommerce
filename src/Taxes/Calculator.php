a74835aa92c335c5c38c870775976736d3defeaf
badrman1983 <bbadrman@gmail.com>
2022-09-16 23:47:20 +0100

    voir container de service

--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -2,9 +2,20 @@
 
 namespace App\Taxes;
 
+use Psr\Log\LoggerInterface;
+
+
 
 class Calculator {
+
+protected $logger;
+
+public function __construct(LoggerInterface $logger)    {
+    $this->logger = $logger;
+    
+}
     public function calcul(float $prix): float {
+        $this->logger->info("un calcul a lieu : $prix");
         return $prix * (20/100);
     }
 }
