ffd50602e30cf43e2da7e3dad8840a688b18113c
badrman1983 <bbadrman@gmail.com>
2022-09-24 20:46:31 +0100

    12-Créer des jeux de fausses données avec les Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($p = 0; $p < 100; $p++  ){
+            $product = new Product();
+            $product->setName("Product n°$p")
+                    ->setPrice(mt_rand(100, 200))
+                    ->setslug("produit-n-$p");
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
