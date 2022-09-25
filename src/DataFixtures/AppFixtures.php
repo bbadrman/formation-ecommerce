ee18ebc2a77f35ee8d3d3a867e580f38ba9dbacf
badrman1983 <bbadrman@gmail.com>
2022-09-25 22:51:16 +0100

    Créer de véritables Slugs avec le composant String

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,9 +6,16 @@ use Faker\Factory;
 use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger){
+            $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create("fr_FR");
@@ -20,7 +27,7 @@ class AppFixtures extends Fixture
             $product = new Product();
             $product->setName($faker->productName)
                     ->setPrice($faker->price(4000, 20000))
-                    ->setslug($faker->slug());
+                    ->setslug(strtolower($this->slugger->slug($product->getName())));
 
             $manager->persist($product);
         }
