e80bb6ef67089ac71a052a9ba6d881f52f5659ba
badrman1983 <bbadrman@gmail.com>
2022-09-25 22:36:43 +0100

    Enrichir Faker pour plus de réalisme encore

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,11 +12,14 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create("fr_FR");
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
 
         for($p = 0; $p < 100; $p++  ){
             $product = new Product();
-            $product->setName($faker->sentence())
-                    ->setPrice(mt_rand(100, 200))
+            $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
                     ->setslug($faker->slug());
 
             $manager->persist($product);
