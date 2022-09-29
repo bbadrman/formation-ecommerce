39872f28e8ead67f0e505c73654277a83f8cc019
badrman1983 <bbadrman@gmail.com>
2022-09-29 20:02:51 +0100

    datatransformer-factoriser-le-code-dans-une-classe

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,81 +21,96 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('name', TextType::class, [
-            'label' => 'Nom du produit',
-            'attr' => [
-                
-                'placeholder' => 'Tapez le Nom du produit',
-            ] 
-        ])
-                ->add('shortDescription', TextareaType::class, [
-                    'label' => 'Description courte',
-                    'attr' => [
-                      
-                        'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur',
-                    ] 
-                ]) 
-                ->add('price', MoneyType::class, [
-                    'label' => 'Prix du product',
-                    'attr' => [
-                       
-                        'placeholder' => 'Tapez le prix en Dhs',
-                    ] 
-                    ])
-                ->add('mainPicture', UrlType::class, [
-                    'label' => 'Main picture',
-                    'attr' => [
-                    'placeholder' => 'Taper une url d\'image !']
-                ])
-                ->add('category', EntityType::class, [
-                    'label' => 'Catégorie',
-                     
-                        'placeholder' => '-- choisir une catégorie --',
-                        'class' => Category::class,
-                        'choice_label' => function(Category $category) {
-                            return strtoupper($category->getName());
-                        }
-                    
-                    ])
-                ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+                'attr' => [
 
-         $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
-           $product = $event->getData();
+                    'placeholder' => 'Tapez le Nom du produit',
+                ]
+            ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => [
 
-           if ($product->getPrice() !== null) {
-              $product->setPrice($product->getPrice() * 100 );
+                    'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur',
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du product',
+                'attr' => [
 
-           }
-         });       
+                    'placeholder' => 'Tapez le prix en Dhs',
+                ]
+            ])
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'Main picture',
+                'attr' => [
+                    'placeholder' => 'Taper une url d\'image !'
+                ]
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
 
-         $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
-            $form = $event->getForm();
+                'placeholder' => '-- choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => function (Category $category) {
+                    return strtoupper($category->getName());
+                }
 
-            /** @var Product */
+            ]);
 
-            $product = $event->getData();
 
-            if($product->getPrice() !== null) {
-                
-                $product->setPrice($product->getPrice() / 100);
+        $builder->get('price')->addModelTransformer(new CallbackTransformer(
+            function ($value) {
+                if ($value === null) {
+                    return;
+                }
+                return $value / 100;
+            },
+            function ($value) {
+                if ($value === null) {
+                    return;
+                }
+                return $value * 100;
             }
-          
+        ));
+        //  $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+        //    $product = $event->getData();
 
-            // if ($product->getId() === null) {
-            //     $form->add('category', EntityType::class, [
-            //         'label' => 'Catégorie',
-                     
-            //             'placeholder' => '-- choisir une catégorie --',
-            //             'class' => Category::class,
-            //             'choice_label' => function(Category $category) {
-            //                 return strtoupper($category->getName());
-            //             }
-                    
-            //         ]);
-            // }
-         });
+        //    if ($product->getPrice() !== null) {
+        //       $product->setPrice($product->getPrice() * 100 );
 
-    
+        //    }
+        //  });       
+
+        //  $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+
+        //     $product = $event->getData();
+
+        //     if($product->getPrice() !== null) {
+
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+
+
+        // if ($product->getId() === null) {
+        //     $form->add('category', EntityType::class, [
+        //         'label' => 'Catégorie',
+
+        //             'placeholder' => '-- choisir une catégorie --',
+        //             'class' => Category::class,
+        //             'choice_label' => function(Category $category) {
+        //                 return strtoupper($category->getName());
+        //             }
+
+        //         ]);
+        // }
+        //  });
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
