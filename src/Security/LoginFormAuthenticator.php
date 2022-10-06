50225f41b23b923e6652d5acc856ed439b4b9292
badrman1983 <bbadrman@gmail.com>
2022-10-06 17:46:51 +0100

    Authenticator compléter la procédure d'authentification

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -4,6 +4,8 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -11,6 +13,12 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class LoginFormAuthenticator extends AbstractGuardAuthenticator
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+         $this->encoder = $encoder;
+    }
+
     public function supports(Request $request)
     {
         return $request->attributes->get('_route') === 'login_security'
@@ -19,17 +27,22 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function getCredentials(Request $request)
     {
-        // todo
+        //verifie que le vrais route
+        return $request->request->get('login');
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        // verifie le email correspondant
+        return $userProvider->loadUserByUsername($credentials['email']);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        // verifier que le mot de pass tapez correspondant on du base données
+        //$credentials['password'] = $user->getPassword();
+
+    return $this->encoder->isPasswordValid($user, $credentials['password']);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
@@ -39,7 +52,7 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        // todo
+       dd('succes');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
