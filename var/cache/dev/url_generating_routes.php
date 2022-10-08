6a304c5783ce010f8f6008f249a1d386873a8ffb
badrman1983 <bbadrman@gmail.com>
2022-10-09 00:17:43 +0100

    Afficher l'état du panier dans une page

--- var/cache/dev/url_generating_routes.php
+++ var/cache/dev/url_generating_routes.php
@@ -16,6 +16,7 @@ return [
     '_profiler_exception' => [['token'], ['_controller' => 'web_profiler.controller.exception_panel::body'], [], [['text', '/exception'], ['variable', '/', '[^/]++', 'token', true], ['text', '/_profiler']], [], []],
     '_profiler_exception_css' => [['token'], ['_controller' => 'web_profiler.controller.exception_panel::stylesheet'], [], [['text', '/exception.css'], ['variable', '/', '[^/]++', 'token', true], ['text', '/_profiler']], [], []],
     'cart_add' => [['id'], ['_controller' => 'App\\Controller\\CartController::add'], ['id' => '\\d+'], [['variable', '/', '\\d+', 'id', true], ['text', '/cart/add']], [], []],
+    'cart_show' => [[], ['_controller' => 'App\\Controller\\CartController::show'], [], [['text', '/cart']], [], []],
     'category_create' => [[], ['_controller' => 'App\\Controller\\CategoryController::create'], [], [['text', '/admin/category/create']], [], []],
     'category_edit' => [['id'], ['_controller' => 'App\\Controller\\CategoryController::edit'], [], [['text', '/edit'], ['variable', '/', '[^/]++', 'id', true], ['text', '/admin/category']], [], []],
     'hello' => [['nom'], ['nom' => 'world', '_controller' => 'App\\Controller\\HelloController::hello'], [], [['variable', '/', '[^/]++', 'nom', true], ['text', '/hello']], [], []],
