2b3608c65791f903b90a0dd9bbdaee4f2cc4cd9e
badrman1983 <bbadrman@gmail.com>
2022-10-08 21:25:09 +0100

    Découverte des bags et du FlashBag

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
@@ -18,7 +19,7 @@ class CartController extends AbstractController
     {
         //0. Securisation :est ce que le produit existe?
 
-        $product= $productRepository->find($id);
+        $product = $productRepository->find($id);
 
         if (!$product) {
             throw $this->createNotFoundException("Produit $id n'existe pas");
@@ -31,23 +32,25 @@ class CartController extends AbstractController
         //3. voir si le produit ($id) existe déjà dans le tableau 
         //4. Si c'est le cas, simplement augmenter la quantité
         //5. Sinon, ajouter le produit avec la quantité 1
-        if(array_key_exists($id, $cart)){
+        if (array_key_exists($id, $cart)) {
             $cart[$id]++;
-        }else{
+        } else {
             $cart[$id] = 1;
         }
         //6. Enregistrer le tableau mis à jour dans la session
 
         $session->set('cart', $cart);
-        dd($session->get('cart'));
+        // dd($session->get('cart'));*
+
+        /** @var FlashBag */
+        $flashBag = $session->getBag('flashes');
+        $flashBag->add('success', "Le produit à bien été ajouté au panier ");
 
         // $request->getSession()->remove('cart');
-        
+
         return $this->redirectToRoute('product_show', [
             'category_slug' => $product->getCategory()->getSlug(),
             'slug' => $product->getSlug()
         ]);
-
-
     }
 }
