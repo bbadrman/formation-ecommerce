2cc2e8f75bfcb5c119d63648ca9691c4d129c098
badrman1983 <bbadrman@gmail.com>
2022-10-12 20:17:57 +0100

    Formulaire de commande

---
+++ src/Controller/PurchaseConfirmeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+Class PurchaseConfirmeController{
+
+    /**
+     *@Route("/purchase/confirme", name="purchase_confirm")
+     */
+    public function confirm(){
+
+    }
+
+}
