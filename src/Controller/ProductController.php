99656740ffc3596735590f908fc3d25344c5a809
badrman1983 <bbadrman@gmail.com>
2022-10-15 19:15:39 +0100

    Création de notre premier Listener

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -39,8 +39,10 @@ class ProductController extends AbstractController
     /**
      * @Route("/{category_slug}/{slug}", name="product_show", priority=-1)
      */
-    public function show($slug, ProductRepository $productRepository)
+    public function show($slug, $prenom, ProductRepository $productRepository, Request $request)
     {
+
+        dd($prenom);
         $product = $productRepository->findOneBy([
             'slug' => $slug,
         ]);
