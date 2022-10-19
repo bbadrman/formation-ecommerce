4ec9afa98446b6ee717d7c1d9356b8c3e6855c61
badrman1983 <bbadrman@gmail.com>
2022-10-19 20:04:35 +0100

    Envoyer un email de confirmation lors d'une commande

--- src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
+++ src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
@@ -4,6 +4,10 @@ namespace App\EventDispatcher;
 
 use Psr\Log\LoggerInterface;
 use App\Event\PurchaseSuccessEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 
@@ -11,10 +15,14 @@ class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
 {
 
     protected $logger;
-    public function __construct(LoggerInterface $logger){
+    protected $mailer;
+    protected $security;
+
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer, Security $security){
 
         $this->logger = $logger;
-        
+        $this->mailer = $mailer;
+        $this->security = $security;
     }
 
     public static function getSubscribedEvents(){
@@ -25,6 +33,23 @@ class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
 
     public function sendSuccessEmail(PurchaseSuccessEvent $purchaseSuccessEvent)
     {
+        //1. récupérer l'utilisateur actuellement en ligne (pour connaitre son email grace a security)
+        /** @var User */
+        $currentUser = $this->security->getUser();
+        //2. récupérer la commande (je la trouverai dans PurchaseSuccessEvent)
+        $purchase = $purchaseSuccessEvent->getPurchase();
+        //3. Ecrire le mail (nouveau TemplatedEmail)
+        $email = new TemplatedEmail();
+        $email->to(new Address($currentUser->getEmail(), $currentUser->getFullName()))
+              ->from("contact@gmail.com")
+              ->subject("Bravo, votre commande ({$purchase->getId()}) a bien été confirmée")
+              ->htmlTemplate('emails/purchase_success.html.twig')
+              ->context([
+                'purchase' => $purchase,
+                'user' => $currentUser
+              ]);
+        //4.Enovoyer l'email grace a MailerInterface
+        $this->mailer->send($email);
         $this->logger->info("Email envoyé pour la commande n°" .$purchaseSuccessEvent->getPurchase()->getId());
     }
 }
